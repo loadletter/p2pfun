5d4b3a0cad717d379057a25df9eb02220274cffa
loadletter <loadletter@users.noreply.github.com>
2014-06-01 12:15:03 +0200

    Fixed

--- tbpscraper/tpbscraper.py
+++ tbpscraper/tpbscraper.py
@@ -1,6 +1,7 @@
 import sys
 from multiprocessing.pool import ThreadPool
 import requests
+from BeautifulSoup import BeautifulSoup
 from sqlitedict import SqliteDict
 
 HTTP_PROXY = ""
@@ -9,35 +10,6 @@ NUM_THREADS = 20
 NUM_CHUNK = 200
 
 
-
-def crawl(startid, endid, d):
-	proxies = {}
-	if HTTP_PROXY:
-		proxies['http'] = HTTP_PROXY
-	sess = requests.Session(proxies=proxies)
-	i = startid
-	while i <= endid:
-		pool = ThreadPool(NUM_THREADS)
-		print "queue: %i ==> %i" % (i, i + NUM_CHUNK)
-		for j in xrange(i, i + NUM_CHUNK):
-			if j > endid:
-				break
-			pool.apply_async(downloader, (sess, d, j))
-			i += 1
-		pool.close()
-		pool.join()
-		d.commit()
-	d.close()	
-		
-def downloader(session, db, tid):
-	req = session.get("%s/torrent/%i" % (TPB_URL, tid), timeout=120)
-	if req.status_code == 200 and not "<title>Not Found | The Pirate Bay" in req.text:
-		db[str(tid)] = page_parse(req.text)
-	elif req.status_code == 200:
-		print "torrent/%i: Not found" % tid
-	else:
-		print "torrent/%i: %i %s" % (tid, req.status_code, req.error)
-	
 def page_parse(data):
 	soup = BeautifulSoup(data)
 	dl1 = soup.findAll('dl', {'class' : 'col1'})[0]
@@ -50,10 +22,47 @@ def page_parse(data):
 	leechers = dl2.findAll('dd')[3].text
 	magnet = soup.findAll('a', {'title' : 'Get this torrent'})[0]['href']
 	return {'title': title, 'user' : byuser, 'date' : dateup, 'seeders' : seeders, 'leechers' : leechers, 'magnet' : magnet}
+	
+class TPBScraper:
+	def __init__(self, dbpath, startid, endid):
+		self.database = SqliteDict(dbpath, tablename="tpb", autocommit=False)
+		self.startid = startid
+		self.endid = endid
+		proxies = {}
+		if HTTP_PROXY:
+			proxies['http'] = HTTP_PROXY
+		self.session = requests.Session(proxies=proxies)
+		
+	def crawl(self):
+		i = self.startid
+		while i <= self.endid:
+			pool = ThreadPool(NUM_THREADS)
+			print "queue: %i ==> %i" % (i, i + NUM_CHUNK)
+			for j in xrange(i, i + NUM_CHUNK):
+				if j > self.endid:
+					break
+				pool.apply_async(self.downloader, (j,))
+				i += 1
+			pool.close()
+			pool.join()
+			self.database.commit()
+		self.database.commit()
+		self.database.close()
+			
+	def downloader(self, tid):
+		req = self.session.get("%s/torrent/%i" % (TPB_URL, tid), timeout=120)
+		isfound = not "<title>Not Found | The Pirate Bay" in req.text
+		if req.status_code == 200 and isfound:
+			print "found:", tid
+			self.database[str(tid)] = page_parse(req.text)
+		elif req.status_code == 404 or not isfound:
+			print "torrent/%i: Not found" % tid
+		else:
+			print "torrent/%i: %i %s" % (tid, req.status_code, req.error)
 
 if __name__ == "__main__":
 	if len(sys.argv) == 4:
-		database = SqliteDict(sys.argv[1], tablename="tpb", autocommit=False)
-		crawl(int(sys.argv[2]), int(sys.argv[3]), database)
+		s = TPBScraper(sys.argv[1], int(sys.argv[2]), int(sys.argv[3]))
+		s.crawl()
 	else:
 		print "usage: ", sys.argv[0], "database startid endid"
