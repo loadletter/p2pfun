e0ce6f9f9490bee25222ca5c283709f5dba4979e
loadletter <loadletter@users.noreply.github.com>
2014-07-13 17:09:23 +0200

    Working
    Rewrote to use another pool that doesn't ignore exceptions
    Get dynamically the data from the fields

--- tbpscraper/tpbscraper.py
+++ tbpscraper/tpbscraper.py
@@ -2,6 +2,7 @@ import sys, json
 import requests
 from BeautifulSoup import BeautifulSoup
 from sqlitedict import SqliteMultithread
+from tpool import ThreadPool
 
 HTTP_PROXY = ""
 TPB_URL = "http://uj3wazyk5u4hnvtk.onion"
@@ -9,24 +10,39 @@ SKIP_EXISTING = True
 NUM_THREADS = 20
 NUM_CHUNK = 200
 
+def find_tag(tagname, alt, *dls):
+	for d in dls:
+		for i, r in enumerate(d.findAll('dt')):
+			if tagname in r.text:
+				if type(alt) == str:
+					return d.findAll('dd')[i].text
+				else:
+					return d.findAll('dd')[i]
+	return alt
 
 def page_parse(data):
 	soup = BeautifulSoup(data)
 	dl1 = soup.findAll('dl', {'class' : 'col1'})[0]
-	size = dl1.findAll('dd')[2].text.replace('&nbsp;',' ')
-	category = dl1.findAll('a', {'title' : 'More from this category'})[0].text.replace(' &gt; ', '>')
-	tags = []
-	if len(dl1.findAll('dd')) > 3:
-		tags = map(lambda x: x.text, dl1.findAll('dd')[3].findAll('a'))
-	title = soup.find(id='title').text
 	dl2 = soup.findAll('dl', {'class' : 'col2'})[0]
-	dateup = dl2.findAll('dd')[0].text
-	byuser = dl2.findAll('dd')[1].text
-	seeders = dl2.findAll('dd')[2].text
-	leechers = dl2.findAll('dd')[3].text
+	size = find_tag("Size:", '', dl1, dl2).replace('&nbsp;',' ')
+	category = soup.findAll('a', {'title' : 'More from this category'})[0].text.replace(' &gt; ', '>')
+	tags = []
+	rawtags = find_tag("Tag(s):", None, dl1, dl2)
+	if rawtags:
+		tags = map(lambda x: x.text, rawtags.findAll('a'))
+	title = soup.find(id='title').text
+	dateup = find_tag("Uploaded:", '', dl1, dl2)
+	byuser = find_tag("By:", None, dl1, dl2)
+	user = ''
+	if byuser.findAll('a'):
+		user = byuser.findAll('a')[0].text
+	elif byuser.findAll('i'):
+		user = byuser.findAll('i')[0].text
+	seeders = find_tag("Seeders:", '0', dl1, dl2)
+	leechers = find_tag("Leechers:", '0', dl1, dl2)
 	magnet = soup.findAll('a', {'title' : 'Get this torrent'})[0]['href'].split('&')[0]
 	comment = soup.findAll('div', {'class' : 'nfo'})[0].text
-	return (title, byuser, dateup, seeders, leechers, comment, magnet, category, json.dumps(tags))
+	return (title, user, dateup, int(seeders), int(leechers), comment, magnet, category, json.dumps(tags))
 	
 class TPBScraper:
 	def __init__(self, dbpath, startid, endid):
@@ -39,6 +55,9 @@ class TPBScraper:
 		if HTTP_PROXY:
 			proxies['http'] = HTTP_PROXY
 		self.session = requests.Session(proxies=proxies)
+	
+	def exists(self, tid):
+		return self.database.select_one('SELECT 1 FROM tpb WHERE tid = ?', (tid,)) is not None
 		
 	def crawl(self):
 		i = self.startid
@@ -48,41 +67,27 @@ class TPBScraper:
 			for j in xrange(i, i + NUM_CHUNK):
 				if j > self.endid:
 					break
-				
-				#dlthreads = []
-				#for n in range(0, DOWN_THREADS):
-					#DlThread = Thread(target=worker, args=(namelist, resultlist))
-					#DlThread.daemon=True
-					#DlThread.start()
-					#dlthreads.append(DlThread)
-				#for dlthread in dlthreads:
-					#dlthread.join()
-
-				#or
-				
-				#start thread
-				#while dlthreads > NUM_TREADS: sleep 1
-
-
+				pool.add_task(self.downloader, j)
 				i += 1
-			pool.close()
-			pool.join()
+			pool.wait_completion()
 			self.database.commit()
 		self.database.commit()
 		self.database.close()
 			
 	def downloader(self, tid):
-		if SKIP_EXISTING and str(tid) in self.database:
+		if SKIP_EXISTING and self.exists(tid):
 			return
-		req = self.session.get("%s/torrent/%i" % (TPB_URL, tid), timeout=120, headers={"accept-language": "en"})
-		isfound = not "<title>Not Found | The Pirate Bay" in req.text
-		if req.status_code == 200 and isfound:
-			print "found:", tid
-			self.database.execute('INSERT OR REPLACE INTO tpb (tid, title, user, date, seeders, leechers, comment, magnet, category, tags) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', (tid,) + page_parse(req.text))
-		elif req.status_code == 404 or not isfound:
-			print "torrent/%i: Not found" % tid
-		else:
-			print "torrent/%i: %i %s" % (tid, req.status_code, req.error)
+		try:
+			req = self.session.get("%s/torrent/%i" % (TPB_URL, tid), timeout=120, headers={"accept-language": "en"})
+			if req.status_code == 200:
+				print "found:", tid
+				self.database.execute('INSERT OR REPLACE INTO tpb (tid, title, user, date, seeders, leechers, comment, magnet, category, tags) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', (tid,) + page_parse(req.text))
+			elif req.status_code == 404:
+				print "torrent/%i:NotFound" % tid
+			else:
+				print "torrent/%i: %i %s" % (tid, req.status_code, req.error)
+		except Exception, e:
+			print "torrent/%i:Exception %s" % repr(e)
 
 if __name__ == "__main__":
 	if len(sys.argv) == 4:
