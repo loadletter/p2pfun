3ba72b7ec024bc50f723c9ff6807a4b34c3374b3
loadletter <loadletter@users.noreply.github.com>
2014-08-19 23:31:26 +0200

    Added stuff

---
+++ dhtcrawl/crawl_pg.py
@@ -0,0 +1,7 @@
+import psycopg2
+import dht
+
+DB_EXEC_INIT = """CREATE TABLE IF NOT EXISTS magnets (mid SERIAL UNIQUE, magnet CHAR(64) NOT NULL PRIMARY KEY, lastupdated INTEGER)
+CREATE TABLE IF NOT EXISTS addrmap (mid INTEGER REFERENCES magnets(mid) ON DELETE CASCADE, aid INTEGER REFERENCES addresses(aid) ON DELETE CASCADE, firstseen INTEGER, lastseen INTEGER, PRIMARY KEY(mid, aid))
+CREATE TABLE IF NOT EXISTS addresses (aid SERIAL UNIQUE, ipaddr INET NOT NULL PRIMARY KEY, firstseen INTEGER, lastseen INTEGER)"""
+
