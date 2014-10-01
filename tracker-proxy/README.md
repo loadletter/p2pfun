e9a8ff25a125aff202a8a1926bb35aec3d0222c3
loadletter <loadletter@users.noreply.github.com>
2014-10-01 22:14:06 +0200

    Done

---
+++ tracker-proxy/README.md
@@ -0,0 +1,15 @@
+tracker-proxy
+=============
+
+```
+Your torrent client is not authorized for the use on this tracker
+```
+
+Well fuck you
+
+1. Edit TRACKER in the script with the destination tracker
+2. Start the script
+3. Add to the torrent 'http://localhost:8025/announce' as tracker
+4. Enjoy freedom
+
+Currently requires python 2 and python-requests
--- tracker-proxy/trackproxy.py
+++ tracker-proxy/trackproxy.py
@@ -1,58 +1,46 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import os, SimpleHTTPServer, SocketServer, socket, cgi, urlparse, requests, urllib
-
+import SimpleHTTPServer, SocketServer, socket, urlparse, requests, re
+########################################################################
 TRACKER = 'http://october.bakabt.me:2710/cdf9a433d3e2c0f89862f68bc8d23aaf/announce.php'
 PORT = 8025
 HTTP_UA = 'uTorrent/2210(25302)'
 PEER_ID = '-UT2210-%d6b%cb%d3R%d9%c7%e3%bcvp%86'
+########################################################################
 HOSTNAME = socket.gethostbyaddr(socket.gethostname())[0]
 ANNOUNCE_URL = TRACKER
 BASE_URL, ANNOUNCE_KW = ANNOUNCE_URL.rsplit('/', 1)
 SCRAPE_URL = ''.join((BASE_URL, '/', 'scrape', ANNOUNCE_KW.replace('announce', '')))
+REQ_HEADERS = {'User-Agent' : HTTP_UA, 'Connection' : 'close', 'Accept-Encoding' : 'gzip'}
 
 class webDispatcher(SimpleHTTPServer.SimpleHTTPRequestHandler):
-
-	def req_hello(self):
+	def req_hello(self, qs):
 		self.send_response(200)
 		self.send_header("Content-Type","text/html")
 		self.end_headers()       
 		self.wfile.write('Hello.')
 
-	def req_announce(self, **kwargs):
-		keys = tuple(kwargs)
-		values = map(lambda x: kwargs[x][0], keys)
-		args = dict(zip(keys, values))
-		args['peer_id'] = PEER_ID
-		#re.sub('peer_id=[^&]+',  'asdasd', url)
-		print kwargs
-		print args
-		dest_url = ANNOUNCE_URL + '?' + urllib.urlencode(args)
-		print dest_url
-		req = requests.get(dest_url, headers={'User-Agent' : HTTP_UA})
-		print req.url
+	def req_announce(self, qs):
+		newquery = re.sub('peer_id=[^&]+', 'peer_id=' + PEER_ID, qs)
+		req = requests.get(ANNOUNCE_URL + '?' + newquery, headers=REQ_HEADERS)
 		self.send_response(req.status_code)
 		self.end_headers()
 		self.wfile.write(req.content)
 
-	def req_scrape(self, **kwargs):
-		keys = tuple(kwargs)
-		values = map(lambda x: kwargs[x][0], keys)
-		args = dict(zip(keys, values))
-		if peer_id in args:
-			del args['peer_id']
-		req = requests.get(SCRAPE_URL + '?' + urllib.urlencode(args), headers={'User-Agent' : HTTP_UA})
+	def req_scrape(self, qs):
+		newquery = re.sub('peer_id=[^&]+', '', qs)
+		req = requests.get(SCRAPE_URL + '?' + newquery, headers=REQ_HEADERS)
 		self.send_response(req.status_code)
 		self.end_headers()
 		self.wfile.write(req.content)
 			
 	def do_GET(self):
-		params = cgi.parse_qs(urlparse.urlparse(self.path).query)
-		action = urlparse.urlparse(self.path).path[1:]
+		url_parsed = urlparse.urlparse(self.path)
+		action = url_parsed.path[1:]
 		if action=="": action="hello"
 		methodname = "req_"+action
 		try:
-			getattr(self, methodname)(**params)
+			getattr(self, methodname)(url_parsed.query)
 		except AttributeError:
 			self.send_response(404)
 			self.send_header("Content-Type","text/html")
@@ -65,5 +53,5 @@ class webDispatcher(SimpleHTTPServer.SimpleHTTPRequestHandler):
 			self.wfile.write("400 - Bad request")
 		  
 httpd = SocketServer.ThreadingTCPServer(('', PORT), webDispatcher)
-print u"Server listening at http://%s:%s" % (HOSTNAME,PORT)
+print u"Server listening at http://%s:%s" % (HOSTNAME, PORT)
 httpd.serve_forever()
